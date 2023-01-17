5d716a5362528ccaa356efd1d4e4cf4cb30a786b
Christoph Henrici <chhenrici@gmail.com>
2023-01-17 15:47:17 +0100

    deal with empty config

--- scripts/build_all/command_line.py
+++ scripts/build_all/command_line.py
@@ -7,6 +7,8 @@ from pathlib import Path
 
 def remove_module_paths(modules_w_path):
     modules = []
+    if len(module_w_path) == 0:
+        return modules
     for module_w_path in modules_w_path:
         module , module_path = module_w_path.split(":")
         if module not in modules:
