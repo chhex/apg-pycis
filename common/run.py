1799d128f2584e01f099d2337f1466400e8ad313
Christoph Henrici <chhenrici@gmail.com>
2023-02-09 17:04:36 +0100

    call instead of run subprocess

--- common/run.py
+++ common/run.py
@@ -13,3 +13,15 @@ def run_subprocess(cmd, env =  os.environ.copy(), verbose = False):
     if output.returncode != 0:
         print("Programm Error termindated because of failing subprocess")
         exit(1)
+
+def call_subprocess(cmd, env =  os.environ.copy(), verbose = False):
+    if verbose:
+        print("About to run command: %s " % (" ".join(cmd)))
+    output = subprocess.call(cmd, env=env)
+    if verbose:
+        print(output.stdout)
+    if output.stderr:
+        print(output.stderr)
+    if output.returncode != 0:
+        print("Programm Error termindated because of failing subprocess")
+        exit(1)
