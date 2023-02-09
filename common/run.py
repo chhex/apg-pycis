a429a853931e124d52e16ea95e39b4fcb2bcfe70
Christoph Henrici <chhenrici@gmail.com>
2023-02-09 17:06:36 +0100

    call rt

--- common/run.py
+++ common/run.py
@@ -17,11 +17,7 @@ def run_subprocess(cmd, env =  os.environ.copy(), verbose = False):
 def call_subprocess(cmd, env =  os.environ.copy(), verbose = False):
     if verbose:
         print("About to run command: %s " % (" ".join(cmd)))
-    output = subprocess.call(cmd, env=env)
-    if verbose:
-        print(output.stdout)
-    if output.stderr:
-        print(output.stderr)
-    if output.returncode != 0:
+    rt = subprocess.call(cmd, env=env)
+    if rt != 0:
         print("Programm Error termindated because of failing subprocess")
         exit(1)
